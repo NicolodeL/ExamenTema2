7e4e2e9d6f6d130e6ed7af1fbd19f150fd2fd678
NlodeL <nlodelop@myuax.com>
2023-12-14 12:56:24 +0100

    añado ayudandome de proyectos anteriores la estucutra asistencia

---
+++ asistencia.cpp
@@ -0,0 +1,32 @@
+#ifndef ASISTENCIA_H
+#define ASISTENCIA_H
+
+#include "estudiante.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Declaración de la estructura Asistencia
+struct Asistencia {
+    std::string fecha;
+    std::string materia;
+    std::string estado;
+};
+
+// Declaración de la clase Estudiante
+class Estudiante {
+public:
+    Estudiante(const std::string& nombre, int edad, float promedio);
+
+    // Funciones para la asistencia
+    void registrarAsistencia(const std::string& fecha, const std::string& materia, const std::string& estado);
+    void mostrarAsistencia();
+
+private:
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<Asistencia> asistencias;
+};
+
+#endif // ASISTENCIA_H
